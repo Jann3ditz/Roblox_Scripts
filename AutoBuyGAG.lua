f31af9741ef9503a5a6cddb97c62eef23c17df9c
Jann3ditz <kuyajan20@gmail.com>
2025-07-24 07:47:19 +0800

    Update AutoBuyGAG.lua

--- AutoBuyGAG.lua
+++ AutoBuyGAG.lua
@@ -245,14 +245,14 @@ end
 local EventShopUI = player.PlayerGui:FindFirstChild("EventShop_UI")
 local dailyUI = player.PlayerGui:FindFirstChild("DailyQuests_UI")
 local merchantUI = player.PlayerGui:FindFirstChild("TravelingMerchantShop_UI")
-local SeedShop = player.PlayerGui:FindFirstChild("SeedShop")
-local GearShop = player.PlayerGui:FindFirstChild("GearShop")
+local SeedShop = player.PlayerGui:FindFirstChild("SeedShop_UI")
+local GearShop = player.PlayerGui:FindFirstChild("GearShop_UI")
 
 createQuestButton("Tranquil Treasures", 0, EventShopUI)
 createQuestButton("Daily Quest", 1, dailyUI)
 createQuestButton("Travelling Merchant", 2, merchantUI)
-createQuestButton("Seed Shop", 3, SeedShop)
-createQuestButton("Gear Shop", 4, GearShop)
+createQuestButton("Seed Shop", 3, SeedShopUI)
+createQuestButton("Gear Shop", 4, GearShopUI)
 
 -- [MULTISELECT SYSTEM]
 local function createMultiSelectSection(name, items, parent, selectedTable)
